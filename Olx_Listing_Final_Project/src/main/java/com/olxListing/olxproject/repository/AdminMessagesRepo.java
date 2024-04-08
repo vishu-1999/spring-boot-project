1e630aed2661b771ce54c063b64d34222acf9490
vishu-1999 <84626670+vishu-1999@users.noreply.github.com>
2024-04-08 15:33:21 +0530

    Delete Olx_Listing_Final_Project directory

--- Olx_Listing_Final_Project/src/main/java/com/olxListing/olxproject/repository/AdminMessagesRepo.java
+++
@@ -1,9 +0,0 @@
-package com.olxListing.olxproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.olxListing.olxproject.entity.AdminMessages;
-
-public interface AdminMessagesRepo extends JpaRepository<AdminMessages, Integer>{
-
-}
--- Olx_Listing_Final_Project/src/main/java/com/olxListing/olxproject/repository/Bookmark_Repo.java
+++
@@ -1,9 +0,0 @@
-package com.olxListing.olxproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.olxListing.olxproject.entity.Bookmark;
-
-public interface Bookmark_Repo extends JpaRepository<Bookmark, Integer>{
-
-}
